4669c80bce6e159151be08a62dddba81d568158c
Lucas Luann Belem de Brito <lucasluann10@gmail.com>
2020-11-29 09:00:24 -0300

    Entrada_de_dados

---
+++ Excript_C/Entrada_de_dados.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int num, num2;
+    printf("Digite um numero: ");
+    scanf("%i", &num);
+
+    printf("Digite outro numero: ");
+    scanf("%i", &num2);
+
+    printf("\n%i\n%i\n", num, num2);
+
+    return 0;
+}
